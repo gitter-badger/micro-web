c24933d64e1c8d96c5ddf77dbeef0eed2b2ca4b4
Thanh Truong <vagabondtt1503@gmail.com>
2015-09-28 11:11:17 -0400

    convert from ES5 to ES6

---
+++ src/app/themes/master/pages/home/home.js
@@ -0,0 +1,10 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'home'
+})
+@View({
+  templateUrl: 'app/themes/master/pages/home/home.html',
+  styleUrls: ['app/themes/master/pages/home/home.css']
+})
+export class Home {}
